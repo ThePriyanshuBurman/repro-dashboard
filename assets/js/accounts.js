1784ca4b791a0815d1e6cceb0dbd19e76ee36f63
ThePriyanshuBUrman <priyanshuburman.it.aec@gmail.com>
2024-09-24 11:39:17 +0530

    first commit

---
+++ assets/js/accounts.js
@@ -0,0 +1,382 @@
+/**
+ * App eCommerce Category List
+ */
+
+'use strict';
+
+// Comment editor
+
+const commentEditor = document.querySelector('.comment-editor');
+
+if (commentEditor) {
+  new Quill(commentEditor, {
+    modules: {
+      toolbar: '.comment-toolbar'
+    },
+    placeholder: 'Write a Comment...',
+    theme: 'snow'
+  });
+}
+
+// Datatable (jquery)
+
+$(function () {
+  let borderColor, bodyBg, headingColor;
+
+  if (isDarkStyle) {
+    borderColor = config.colors_dark.borderColor;
+    bodyBg = config.colors_dark.bodyBg;
+    headingColor = config.colors_dark.headingColor;
+  } else {
+    borderColor = config.colors.borderColor;
+    bodyBg = config.colors.bodyBg;
+    headingColor = config.colors.headingColor;
+  }
+
+  // Variable declaration for category list table
+  var dt_category_list_table = $('.datatables-category-list');
+
+  //select2 for dropdowns in offcanvas
+
+  var select2 = $('.select2');
+  if (select2.length) {
+    select2.each(function () {
+      var $this = $(this);
+      $this.wrap('<div class="position-relative"></div>').select2({
+        dropdownParent: $this.parent(),
+        placeholder: $this.data('placeholder') //for dynamic placeholder
+      });
+    });
+  }
+
+  // Customers List Datatable
+
+  if (dt_category_list_table.length) {
+    var dt_category = dt_category_list_table.DataTable({
+      ajax: assetsPath + 'json/accounts.json', // JSON file to add data
+      columns: [
+        // columns according to JSON
+        { data: '' },
+        { data: 'id' },
+        { data: 'name' },
+        { data: 'shoots' },
+        { data: 'account_type' },
+        { data: 'company' },
+        { data: 'phone' },
+        { data: '' }
+      ],
+      columnDefs: [
+        {
+          // For Responsive
+          className: 'control',
+          searchable: false,
+          orderable: false,
+          responsivePriority: 1,
+          targets: 0,
+          render: function (data, type, full, meta) {
+            return '';
+          }
+        },
+        {
+          // For Checkboxes
+          targets: 1,
+          orderable: false,
+          searchable: false,
+          responsivePriority: 4,
+          checkboxes: true,
+          render: function () {
+            return '<input type="checkbox" class="dt-checkboxes form-check-input">';
+          },
+          checkboxes: {
+            selectAllRender: '<input type="checkbox" class="form-check-input">'
+          }
+        },
+        {
+          // Categories and Category Detail
+          targets: 2,
+          responsivePriority: 2,
+          render: function (data, type, full, meta) {
+            var $name = full['name'],
+              $email = full['email'],
+              $shoots=full['shoots'],
+              $account_type = full['account_type'],
+              $company = full['company'],
+              $id = full['id'];
+            // Creates full output for Categories and Category Detail
+            var $row_output =
+                '<div class="d-flex align-items-center">' +
+                '<div class="d-flex flex-column justify-content-center">' +
+                '<span class="text-heading text-wrap fw-medium">' +
+                $name +
+                '</span>' +
+                '<span class="text-truncate mb-0 d-none d-sm-block"><small>' +
+                $email +
+                '</small></span>' +
+                '</div>' +
+                '</div>';
+            return $row_output;
+          }
+        },
+        {
+            // shoots
+            targets: 3,
+            responsivePriority: 3,
+            render: function (data, type, full, meta) {
+              var $shoots = full['shoots'];
+              return '<div class="text-sm-center justify-content-center">' + $shoots + '</div>';
+            }
+        },
+        {
+            // account type
+            targets: 4,
+            responsivePriority: 3,
+            render: function (data, type, full, meta) {
+              var $account_type = full['account_type'];
+              return '<div class="text-sm-center justify-content-center">' + $account_type + '</div>';
+            }
+        },
+        {
+          // company name
+          targets: 5,
+          responsivePriority: 3,
+          render: function (data, type, full, meta) {
+            var $company = full['company'];
+            return '<div class="text-sm-center justify-content-center">' + $company + '</div>';
+          }
+        },
+        {
+          // phone number
+          targets: 6,
+          orderable: false,
+          render: function (data, type, full, meta) {
+            var $phone = full['phone'];
+            return "<div class='mb-0 text-sm-center justify-content-center'>" + $phone + '</div';
+          }
+        },
+        {
+          // Actions
+          targets: -1,
+          title: 'Actions',
+          searchable: false,
+          orderable: false,
+          render: function (data, type, full, meta) {
+            return (
+              '<div class="d-flex align-items-sm-center justify-content-sm-center">' +
+              '<button class="btn btn-icon btn-text-secondary rounded-pill waves-effect waves-light dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical ti-md"></i></button>' +
+              '<div class="dropdown-menu dropdown-menu-end m-0">' +
+              '<a href="javascript:0;" class="dropdown-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasAccounts">Edit</a>' +
+              '<a href="javascript:0;" class="dropdown-item">Delete</a>' +
+              '</div>' +
+              '</div>'
+            );
+          }
+        }
+      ],
+      order: [2, 'asc'], //set any columns order asc/desc
+      dom:
+        '<"card-header d-flex flex-wrap py-0 flex-column flex-sm-row"' +
+        '<f>' +
+        '<"d-flex justify-content-center justify-content-md-end align-items-baseline"<"dt-action-buttons d-flex justify-content-center flex-md-row align-items-baseline"lB>>' +
+        '>t' +
+        '<"row mx-1"' +
+        '<"col-sm-12 col-md-6"i>' +
+        '<"col-sm-12 col-md-6"p>' +
+        '>',
+      lengthMenu: [7, 10, 20, 50, 70, 100], //for length of menu
+      language: {
+        sLengthMenu: '_MENU_',
+        search: '',
+        searchPlaceholder: 'Search for a Client',
+        paginate: {
+          next: '<i class="ti ti-chevron-right ti-sm"></i>',
+          previous: '<i class="ti ti-chevron-left ti-sm"></i>'
+        }
+      },
+      // Buttons with Dropdown and for offCanvas
+      buttons: [
+        {
+          extend: 'collection',
+          className: 'btn btn-label-secondary dropdown-toggle me-4 waves-effect waves-light',
+          text: '<i class="ti ti-upload me-1 ti-xs"></i>Export',
+          buttons: [
+            {
+              extend: 'print',
+              text: '<i class="ti ti-printer me-2" ></i>Print',
+              className: 'dropdown-item',
+              exportOptions: {
+                columns: [1, 2, 3, 4, 5, 6, 7],
+                format: {
+                  body: function (inner, coldex, rowdex) {
+                    if (inner.length <= 0) return inner;
+                    var el = $.parseHTML(inner);
+                    var result = '';
+                    $.each(el, function (index, item) {
+                      if (item.classList !== undefined && item.classList.contains('product-name')) {
+                        result = result + item.lastChild.firstChild.textContent;
+                      } else if (item.innerText === undefined) {
+                        result = result + item.textContent;
+                      } else result = result + item.innerText;
+                    });
+                    return result;
+                  }
+                }
+              },
+              customize: function (win) {
+                // Customize print view for dark
+                $(win.document.body)
+                  .css('color', headingColor)
+                  .css('border-color', borderColor)
+                  .css('background-color', bodyBg);
+                $(win.document.body)
+                  .find('table')
+                  .addClass('compact')
+                  .css('color', 'inherit')
+                  .css('border-color', 'inherit')
+                  .css('background-color', 'inherit');
+              }
+            },
+            {
+              extend: 'csv',
+              text: '<i class="ti ti-file me-2" ></i>Csv',
+              className: 'dropdown-item',
+              exportOptions: {
+                columns: [1, 2, 3, 4, 5, 6, 7],
+                format: {
+                  body: function (inner, coldex, rowdex) {
+                    if (inner.length <= 0) return inner;
+                    var el = $.parseHTML(inner);
+                    var result = '';
+                    $.each(el, function (index, item) {
+                      if (item.classList !== undefined && item.classList.contains('product-name')) {
+                        result = result + item.lastChild.firstChild.textContent;
+                      } else if (item.innerText === undefined) {
+                        result = result + item.textContent;
+                      } else result = result + item.innerText;
+                    });
+                    return result;
+                  }
+                }
+              }
+            },
+            {
+              extend: 'copy',
+              text: '<i class="ti ti-copy me-2"></i>Copy',
+              className: 'dropdown-item',
+              exportOptions: {
+                columns: [1, 2, 3, 4, 5, 6, 7],
+                format: {
+                  body: function (inner, coldex, rowdex) {
+                    if (inner.length <= 0) return inner;
+                    var el = $.parseHTML(inner);
+                    var result = '';
+                    $.each(el, function (index, item) {
+                      if (item.classList !== undefined && item.classList.contains('product-name')) {
+                        result = result + item.lastChild.firstChild.textContent;
+                      } else if (item.innerText === undefined) {
+                        result = result + item.textContent;
+                      } else result = result + item.innerText;
+                    });
+                    return result;
+                  }
+                }
+              }
+            }
+          ]
+        },
+        // Button for offCanvas
+        {
+          text: '<i class="ti ti-plus me-0 me-sm-1 ti-xs"></i><span class="d-none d-sm-inline-block">Create Account</span>',
+          className: 'add-new btn btn-primary ms-2 ms-sm-0 waves-effect waves-light',
+            attr: {
+              'data-bs-toggle': 'offcanvas',
+              'data-bs-target': '#offcanvasAccounts'
+            }
+        }
+      ],
+      // For responsive popup
+      responsive: {
+        details: {
+          display: $.fn.dataTable.Responsive.display.modal({
+            header: function (row) {
+              var data = row.data();
+              return 'Details of ' + data['categories'];
+            }
+          }),
+          type: 'column',
+          renderer: function (api, rowIdx, columns) {
+            var data = $.map(columns, function (col, i) {
+              return col.title !== '' // ? Do not show row in modal popup if title is blank (for check box)
+                ? '<tr data-dt-row="' +
+                    col.rowIndex +
+                    '" data-dt-column="' +
+                    col.columnIndex +
+                    '">' +
+                    '<td> ' +
+                    col.title +
+                    ':' +
+                    '</td> ' +
+                    '<td class="ps-0">' +
+                    col.data +
+                    '</td>' +
+                    '</tr>'
+                : '';
+            }).join('');
+
+            return data ? $('<table class="table"/><tbody />').append(data) : false;
+          }
+        }
+      }
+    });
+    $('.dt-action-buttons').addClass('pt-0');
+    $('.dataTables_filter').addClass('me-3 mb-sm-6 mb-0 ps-0');
+  }
+
+  // Filter form control to default size
+  // ? setTimeout used for multilingual table initialization
+  setTimeout(() => {
+    $('.dataTables_filter .form-control').removeClass('form-control-sm');
+    $('.dataTables_filter .form-control').addClass('ms-0');
+    $('.dataTables_length .form-select').removeClass('form-select-sm');
+    $('.dataTables_length .form-select').addClass('ms-0');
+  }, 300);
+});
+
+//For form validation
+(function () {
+  const accountsForm = document.getElementById('accountsForm');
+
+  //Add New customer Form Validation
+  const fv = FormValidation.formValidation(accountsForm, {
+    fields: {
+      brandName: {
+        validators: {
+          notEmpty: {
+            message: 'Please enter your name'
+          }
+        }
+      },
+      email: {
+        validators: {
+          notEmpty: {
+            message: 'Please enter email'
+          }
+        }
+      }
+    },
+    plugins: {
+      trigger: new FormValidation.plugins.Trigger(),
+      bootstrap5: new FormValidation.plugins.Bootstrap5({
+        // Use this for enabling/changing valid/invalid class
+        eleValidClass: 'is-valid',
+        rowSelector: function (field, ele) {
+          // field is the field name & ele is the field element
+          return '.mb-6';
+        }
+      }),
+      submitButton: new FormValidation.plugins.SubmitButton(),
+      // Submit the form when all fields are valid
+      // defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
+      autoFocus: new FormValidation.plugins.AutoFocus()
+    }
+  });
+})();
---
+++ assets/js/tour-branding-info.js
@@ -0,0 +1,270 @@
+/**
+ * App eCommerce Category List
+ */
+
+'use strict';
+
+// Comment editor
+
+const commentEditor = document.querySelector('.comment-editor');
+
+if (commentEditor) {
+  new Quill(commentEditor, {
+    modules: {
+      toolbar: '.comment-toolbar'
+    },
+    placeholder: 'Write a Comment...',
+    theme: 'snow'
+  });
+}
+
+// Datatable (jquery)
+
+$(function () {
+  let borderColor, bodyBg, headingColor;
+
+  if (isDarkStyle) {
+    borderColor = config.colors_dark.borderColor;
+    bodyBg = config.colors_dark.bodyBg;
+    headingColor = config.colors_dark.headingColor;
+  } else {
+    borderColor = config.colors.borderColor;
+    bodyBg = config.colors.bodyBg;
+    headingColor = config.colors.headingColor;
+  }
+
+  // Variable declaration for category list table
+  var dt_category_list_table = $('.datatables-category-list');
+
+  //select2 for dropdowns in offcanvas
+
+  var select2 = $('.select2');
+  if (select2.length) {
+    select2.each(function () {
+      var $this = $(this);
+      $this.wrap('<div class="position-relative"></div>').select2({
+        dropdownParent: $this.parent(),
+        placeholder: $this.data('placeholder') //for dynamic placeholder
+      });
+    });
+  }
+
+  // Customers List Datatable
+
+  if (dt_category_list_table.length) {
+    var dt_category = dt_category_list_table.DataTable({
+      ajax: assetsPath + 'json/tour-branding-info.json', // JSON file to add data
+      columns: [
+        // columns according to JSON
+        { data: '' },
+        { data: 'id' },
+        { data: 'name' },
+        { data: 'company' },
+        { data: 'phone' },
+        { data: '' }
+      ],
+      columnDefs: [
+        {
+          // For Responsive
+          className: 'control',
+          searchable: false,
+          orderable: false,
+          responsivePriority: 1,
+          targets: 0,
+          render: function (data, type, full, meta) {
+            return '';
+          }
+        },
+        {
+          // For Checkboxes
+          targets: 1,
+          orderable: false,
+          searchable: false,
+          responsivePriority: 4,
+          checkboxes: true,
+          render: function () {
+            return '<input type="checkbox" class="dt-checkboxes form-check-input">';
+          },
+          checkboxes: {
+            selectAllRender: '<input type="checkbox" class="form-check-input">'
+          }
+        },
+        {
+          // Categories and Category Detail
+          targets: 2,
+          responsivePriority: 2,
+          render: function (data, type, full, meta) {
+            var $name = full['name'],
+              $email = full['email'],
+              $company = full['company'],
+              $id = full['id'];
+            // Creates full output for Categories and Category Detail
+            var $row_output =
+                '<div class="d-flex align-items-center">' +
+                '<div class="d-flex flex-column justify-content-center">' +
+                '<span class="text-heading text-wrap fw-medium">' +
+                $name +
+                '</span>' +
+                '<span class="text-truncate mb-0 d-none d-sm-block"><small>' +
+                $email +
+                '</small></span>' +
+                '</div>' +
+                '</div>';
+            return $row_output;
+          }
+        },
+        {
+          // company name
+          targets: 3,
+          responsivePriority: 3,
+          render: function (data, type, full, meta) {
+            var $company = full['company'];
+            return '<div class="text-sm-end">' + $company + '</div>';
+          }
+        },
+        {
+          // phone number
+          targets: 4,
+          orderable: false,
+          render: function (data, type, full, meta) {
+            var $phone = full['phone'];
+            return "<div class='mb-0 text-sm-end'>" + $phone + '</div>';
+          }
+        },
+        {
+          // Actions
+          targets: -1,
+          title: 'Actions',
+          searchable: false,
+          orderable: false,
+          render: function (data, type, full, meta) {
+            return (
+              '<div class="d-flex align-items-sm-center justify-content-sm-center">' +
+              '<button class="btn btn-icon btn-text-secondary rounded-pill waves-effect waves-light dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical ti-md"></i></button>' +
+              '<div class="dropdown-menu dropdown-menu-end m-0">' +
+              '<a href="javascript:0;" class="dropdown-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvastourBrandingInfo">Edit</a>' +
+              '<a href="javascript:0;" class="dropdown-item">Delete</a>' +
+              '</div>' +
+              '</div>'
+            );
+          }
+        }
+      ],
+      order: [2, 'asc'], //set any columns order asc/desc
+      dom:
+        '<"card-header d-flex flex-wrap py-0 flex-column flex-sm-row"' +
+        '<f>' +
+        '<"d-flex justify-content-center justify-content-md-end align-items-baseline"<"dt-action-buttons d-flex justify-content-center flex-md-row align-items-baseline"lB>>' +
+        '>t' +
+        '<"row mx-1"' +
+        '<"col-sm-12 col-md-6"i>' +
+        '<"col-sm-12 col-md-6"p>' +
+        '>',
+      lengthMenu: [7, 10, 20, 50, 70, 100], //for length of menu
+      language: {
+        sLengthMenu: '_MENU_',
+        search: '',
+        searchPlaceholder: 'Search Category',
+        paginate: {
+          next: '<i class="ti ti-chevron-right ti-sm"></i>',
+          previous: '<i class="ti ti-chevron-left ti-sm"></i>'
+        }
+      },
+      // Button for offcanvas
+      buttons: [
+        {
+          text: '<i class="ti ti-plus ti-xs me-0 me-sm-2"></i><span class="d-none d-sm-inline-block">Create Branding Record</span>',
+          className: 'add-new btn btn-primary ms-2 waves-effect waves-light',
+          attr: {
+            'data-bs-toggle': 'offcanvas',
+            'data-bs-target': '#offcanvastourBrandingInfo'
+          }
+        }
+      ],
+      // For responsive popup
+      responsive: {
+        details: {
+          display: $.fn.dataTable.Responsive.display.modal({
+            header: function (row) {
+              var data = row.data();
+              return 'Details of ' + data['categories'];
+            }
+          }),
+          type: 'column',
+          renderer: function (api, rowIdx, columns) {
+            var data = $.map(columns, function (col, i) {
+              return col.title !== '' // ? Do not show row in modal popup if title is blank (for check box)
+                ? '<tr data-dt-row="' +
+                    col.rowIndex +
+                    '" data-dt-column="' +
+                    col.columnIndex +
+                    '">' +
+                    '<td> ' +
+                    col.title +
+                    ':' +
+                    '</td> ' +
+                    '<td class="ps-0">' +
+                    col.data +
+                    '</td>' +
+                    '</tr>'
+                : '';
+            }).join('');
+
+            return data ? $('<table class="table"/><tbody />').append(data) : false;
+          }
+        }
+      }
+    });
+    $('.dt-action-buttons').addClass('pt-0');
+    $('.dataTables_filter').addClass('me-3 mb-sm-6 mb-0 ps-0');
+  }
+
+  // Filter form control to default size
+  // ? setTimeout used for multilingual table initialization
+  setTimeout(() => {
+    $('.dataTables_filter .form-control').removeClass('form-control-sm');
+    $('.dataTables_filter .form-control').addClass('ms-0');
+    $('.dataTables_length .form-select').removeClass('form-select-sm');
+    $('.dataTables_length .form-select').addClass('ms-0');
+  }, 300);
+});
+
+//For form validation
+(function () {
+  const tourBrandingInfoForm = document.getElementById('tourBrandingInfoForm');
+
+  //Add New customer Form Validation
+  const fv = FormValidation.formValidation(tourBrandingInfoForm, {
+    fields: {
+      brandName: {
+        validators: {
+          notEmpty: {
+            message: 'Please enter your name'
+          }
+        }
+      },
+      email: {
+        validators: {
+          notEmpty: {
+            message: 'Please enter email'
+          }
+        }
+      }
+    },
+    plugins: {
+      trigger: new FormValidation.plugins.Trigger(),
+      bootstrap5: new FormValidation.plugins.Bootstrap5({
+        // Use this for enabling/changing valid/invalid class
+        eleValidClass: 'is-valid',
+        rowSelector: function (field, ele) {
+          // field is the field name & ele is the field element
+          return '.mb-6';
+        }
+      }),
+      submitButton: new FormValidation.plugins.SubmitButton(),
+      // Submit the form when all fields are valid
+      // defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
+      autoFocus: new FormValidation.plugins.AutoFocus()
+    }
+  });
+})();
